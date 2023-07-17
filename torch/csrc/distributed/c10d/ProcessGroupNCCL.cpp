5916d7e4b25ac444428a9588dd009811650e3c70
eqy <eddiey@nvidia.com>
2023-07-16 20:41:17 -0700

    Update ProcessGroupNCCL.cpp

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -839,7 +839,6 @@ void ProcessGroupNCCL::ncclCommWatchdog() {
     auto mode = cudaStreamCaptureModeGlobal;
     cudaThreadExchangeStreamCaptureMode(&mode);
     cudaThreadExchangeStreamCaptureMode(&mode);
-    LOG(INFO) << "[Rank " << rank_ << "] Count: " << count;
     workCleanupLoop();
     VLOG(2) << "[Rank " << rank_
             << "] NCCL watchdog thread terminated normally";
